af2e3e91fca0223d982b98fba63ecb6b7bfa8252
IUL1AN27 <70662088+IUL1AN27@users.noreply.github.com>
2020-09-23 00:30:02 +0300

    Add files via upload

---
+++ Probleme.py
@@ -0,0 +1,45 @@
+#Problema 1
+print("Struguri","100","kg",sep="      ")
+print("Mere","10","tone",sep="       ")
+print("Varza","1000","q",sep="       ")
+#Problema 2
+print("a=3 b=8")
+print("Suma","a+b")
+print("Diferenta","a-b")
+print("Catul","a//b")
+print("Restul","a%b")
+print("Produsul","a*b")
+print("Ridicarea la putere","a**b")
+#Problema 3
+print("l=5")
+print('Perimetrul suprafetei',"4*l")
+print('Aria tututor suprafetelor',"6*l**2")
+print('Volumul',"l**3")
+#Problema 4
+print("n=7")
+print("Transformarea din metri in centimetri","n*100")  
+print("Transformarea din kilograme in miligrame","n*1000000")
+print("Transformarea din ani in luni","n*12")
+print("Transformarea din ani in saptamini","n*52")
+print("Transformarea din ani in zile","n*365")
+#Problema 5
+x=True
+y=False
+print(x and y,x or y,not x,not y,)
+x=False
+x=True
+print(x and y,x or y,not x,not y,)
+x=True
+y=True
+print(x and y,x or y,not x,not y,)
+x=False
+y=False
+print(x and y,x or y,not x,not y,)
+#Problema 6
+n=15
+print("n=15")
+print("Ultima cifra",n%10)
+print("Penultima cifra",n//10)
+print("Restul impartirii",n%9)
+print("Suma cifrelor",n/4)
+print("Rasturnatul",n%10,n//10)
